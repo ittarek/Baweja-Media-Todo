dcebbb793a625247690da7967e0bd89cf6028f2a
MD Tariqul Islam <ittarek551@gmail.com>
2023-09-17 19:14:10 +0600

    all update

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Modal from "./components/Modal"
+import TodoList from "./components/TodoList"
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <TodoList/>
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Modal/>
+    </>
+  )
+}
+
+export default App
